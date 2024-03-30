cf8473cb0f751b45d74ba7320fd98b6acdc654ac
Kedar Basutkar <krb1202@gmail.com>
2024-03-30 11:48:40 +0530

    lint

--- src/App.tsx
+++ src/App.tsx
@@ -48,28 +48,28 @@ const calculateWidth = (scaleFactor: number, width: number): number => {
 }
 
 function App() {
-  const [widthArray, setScaleValue] = useState<number[]>(() => new Array(EL_COUNT).fill(48));
+  const [widthArray, setScaleValue] = useState<number[]>(() => new Array<number>(EL_COUNT).fill(48));
   const conRef = useRef<HTMLDivElement>(null)
 
-  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
     const cursorCenter = Math.max(e.nativeEvent.offsetX, 0)
 
     const res = []
-    
-    for (let i = 0; i < EL_COUNT; i++) {      
-      if(isElementWithinSpan(i, cursorCenter, SPAN, widthArray)) {
+
+    for (let i = 0; i < EL_COUNT; i++) {
+      if (isElementWithinSpan(i, cursorCenter, SPAN, widthArray)) {
         const absDistFromCursorCenter = Math.abs(cursorCenter - calculateAbsoluteCenterDistance(i, widthArray))
-        const absoluteInvertedDistance = Math.abs((SPAN/2) - absDistFromCursorCenter)
+        const absoluteInvertedDistance = Math.abs((SPAN / 2) - absDistFromCursorCenter)
         res.push(calculateWidth(calculateScaleFactor(absoluteInvertedDistance, widthArray[i]), WIDTH))
       } else {
         res.push(WIDTH)
       }
     }
- 
+
     setScaleValue(res)
   }
 
-  const throttledMouseMoveHandler = throttle((e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+  const throttledMouseMoveHandler = throttle((e: React.MouseEvent<HTMLDivElement>) => {
     handleMouseMove(e)
   }, 10);
 
@@ -79,22 +79,20 @@ function App() {
         ref={conRef}
         className="con"
       >
-        {new Array(EL_COUNT).fill("").map((c, idx) => (
-          <Item 
-            key={idx} 
-            index={idx} 
-            width={widthArray[idx]} 
+        {new Array(EL_COUNT).fill("").map((_, idx) => (
+          <Item
+            key={idx}
+            index={idx}
+            width={widthArray[idx]}
           />
         ))}
       </div>
-      <div 
+      <div
         className="catcher"
         onMouseMove={throttledMouseMoveHandler}
-        onMouseLeave={() => setScaleValue(new Array(EL_COUNT).fill(48))}        
+        onMouseLeave={() => {setScaleValue(new Array(EL_COUNT).fill(48))}}
       />
     </div>
-    
-    
   );
 }
 
